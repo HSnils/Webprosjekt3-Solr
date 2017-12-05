b2fb650ad1e2bea47cb5010fa53d31c2f3385758
Anders <anders.thon@hotmail.com>
2017-12-06 00:51:53 +0100

    Index file content.
    Sykt skeptisk på alle disse filene som blir fjerna.

--- pages/upload_handler.php
+++ pages/upload_handler.php
@@ -2,39 +2,22 @@
 	
 	if (!empty($_FILES['userFile']) && $_FILES['userFile']['type'] == 'application/pdf') {
 
-/*		$filepath = 'webproj3/documents/';
-		$filename = $_FILES['userFile']['name'];
-		$ch = curl_init('http://localhost:8983/solr/safety/update/extract?literal.id=1&literal.name=Name&uprefix=attr_&fmap.content=attr_content&defaultField=text&commit=true');
-		curl_setopt ($ch, CURLOPT_POST, 1);
-		curl_setopt ($ch, CURLOPT_POSTFIELDS, array('myfile'=>"@webproj3/documents/assignment.pdf"));
-		$result= curl_exec ($ch);
-		$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		curl_close ( $ch );*/
-
-		//var_dump($result);
-		//echo $http_status;
-		//echo $filepath . $filename;
-		//echo $_FILES['userFile']['tmp_name'];
-
 		var_dump($_FILES['userFile']);
 
-		$uploads_dir = '../solr-6.6.1/uploads';
+		$uploads_dir = '../solr-6.6.1/uploads/';
         $tmp_name = $_FILES["userFile"]["tmp_name"];
         $name = basename($_FILES["userFile"]["name"]);
         move_uploaded_file($tmp_name, "$uploads_dir/$name");
 
 
 		$target_url = "http://localhost:8983/solr/safety/update/extract?commit=true";
-		$file_path = "C:/xampp/htdocs/Webprosjekt3/solr-6.6.1/webproj3/documents/assignment.pdf";
+		$file = file_get_contents($uploads_dir . $name);
 
-		$post = array (
-		   'myFile' => '@' . $uploads_dir . '/' . $name
-		);
 
 		$ch = curl_init ();
 		curl_setopt ( $ch, CURLOPT_URL, $target_url );
 		curl_setopt ( $ch, CURLOPT_POST, 1 );
-		curl_setopt ( $ch, CURLOPT_POSTFIELDS, $post );
+		curl_setopt ( $ch, CURLOPT_POSTFIELDS, $file );
 		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
 		$headers = array();
 		$headers[] = 'Accept: application/pdf';
